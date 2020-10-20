f409da6023cd3e50ffacffc94e5d8cd6d246e5f2
Liam M Baker <lmpbaker93@gmail.com>
2020-10-20 07:08:27 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def Parrot(phrase = "Squawk!")
+def parrot(phrase = "Squawk!")
   puts "#{phrase}"
-  return
+  return "#{phrase}"
 end 
